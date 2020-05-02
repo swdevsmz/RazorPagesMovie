c9ad8111709c39f12ca3de0e8f6631906f3a6ede
swdev.smz@gmail.com <swdev.smz@gmail.com>
2020-05-03 00:51:05 +0900

    データのSeed（投入）まで実装

---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [DisplayName("タイトル")]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("リリース日")]
+        public DateTime ReleaseDate { get; set; }
+        [DisplayName("ジャンル")]
+        public string Genre { get; set; }
+        [DisplayName("価格")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+﻿https://docs.microsoft.com/ja-jp/aspnet/core/tutorials/razor-pages/?view=aspnetcore-3.1
